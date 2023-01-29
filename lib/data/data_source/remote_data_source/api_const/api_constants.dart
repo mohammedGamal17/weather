08dc5dc9ac1f0a76cc59cd5909205aaa9ded2172
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-01-29 14:36:18 +0200

    update

--- lib/data/data_source/remote_data_source/api_const/api_constants.dart
+++ lib/data/data_source/remote_data_source/api_const/api_constants.dart
@@ -1,7 +1,7 @@
-import 'package:dio/dio.dart';
+
 
 class APIConstants {
-  static const String appId = '99f40c2e4455df0a3805434da9794ac3';
+  static const String appId = 'YOUR ID';
   static const String baseUrl =
       'https://api.openweathermap.org/data/2.5/weather';
 
